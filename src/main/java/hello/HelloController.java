e0284412140ab69a778afc234f9c47b3adf8a78e
raycoti <raymond.coti@gmail.com>
2017-09-20 16:19:37 -0500

    initializes file structure

---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/api/hello")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
+
